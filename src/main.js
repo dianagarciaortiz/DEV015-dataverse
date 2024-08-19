aea1b72a5d9d61e273476606e17f83f6e2614284
diana <dggo.juss@gmail.com>
2024-08-19 10:51:24 -0600

    proyecto finalizado con los test unitarios y oas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,43 @@
-//import { example } from './dataFunctions.js';
+import { filterData, filterDatas, sortData } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
+
 const mainConteiner = document.getElementById("root")
+const inputNombre = document.getElementById("filtroNombre")
+const selectArea = document.querySelector("select#areaFiltrado")
+const ordenarDatos = document.querySelector('select[name="sortOrder"]')
+const resetButton = document.querySelector('button#resetButton')
+
 mainConteiner.appendChild(renderItems(data))
+
+inputNombre.addEventListener("input", () => {
+  mainConteiner.innerHTML = ""
+  const dataFiltrada = filterData(data, inputNombre.value)
+  mainConteiner.appendChild(renderItems(dataFiltrada))
+})
+
+
+
+selectArea.addEventListener("change", () => {
+  mainConteiner.innerHTML = ""
+  const selectFiltrado = filterDatas(data, selectArea.value)
+  mainConteiner.appendChild(renderItems(selectFiltrado))
+})
+ordenarDatos.addEventListener("change", () => {
+  mainConteiner.innerHTML = ""
+  const dataOrdenada = sortData(data, ordenarDatos.value)
+  mainConteiner.appendChild(renderItems(dataOrdenada))
+})
+
+resetButton.addEventListener("click", (event) => {
+  event.preventDefault()
+  mainConteiner.innerHTML = ""
+  mainConteiner.appendChild(renderItems(data))
+  inputNombre.value=""
+  ordenarDatos.value="placeholder"
+  selectArea.value="placeholder"
+
+});
 
 
 
